ffb990dc9151aad93e56725d8d9fc0abaee46dd1
Nightmare <72258530+vinaykamal@users.noreply.github.com>
2022-11-05 12:54:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import home from './home/home'
+
+function App() {
+  return (
+    <div>
+      <home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/home.jsx
@@ -0,0 +1,34 @@
+import './home.css'
+const home = () => {
+    return(
+        <div className='i'>
+      <div className='next-page'>
+        
+      </div>
+      <div className='i-left'>
+        <div className='i-left-stuff'>
+        <h2 className='i-left-stuff1'>Hello, We are</h2>
+        <h1 className='i-left-name'>Memory Palace</h1>
+        <h3 className='i-desc'>loreum ipsiumjnkvjdznnA<br/> 
+        dscnkjsdc nkJCXN<br/>
+        djxcnijac nkjcn<br/>
+        hdbdi ajcnaks jdcnksjc ocasojc noasjcnosa osodo sdjn</h3>
+        <div className='i-title'>
+          <div className='i-title-stuff'>
+          <div className='i-title-item'>Memory</div>
+          <div className='i-title-item'>Music</div>
+          <div className='i-title-item'>Images</div>
+          <div className='i-title-item'>Videos</div>
+          <div className='i-title-item'>Notes</div>
+        </div>
+        </div>
+        </div>
+      </div>
+      <div className='right'>
+        
+      </div>  
+    </div>
+    )
+}
+
+export default home
